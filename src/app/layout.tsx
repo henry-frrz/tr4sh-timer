ee9a3d2e83333dd4886d31544aaaa4625aa7beed
Henrique Ferraz <henrique07ferraz@gmail.com>
2026-04-06 18:07:52 -0300

    feat: add font - JetBrains Mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const jetbrainsMono = JetBrains_Mono({
+  variable: '--font-jetbrains-mono',
   subsets: ['latin'],
 })
 
 export const metadata: Metadata = {
-  title: "tr4sh timer",
-};
+  title: 'tr4sh timer',
+}
 
 export default function RootLayout({
   children,
@@ -22,10 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang='en'
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang='en' className={`${jetbrainsMono.variable} h-full antialiased`}>
       <body className='min-h-full flex flex-col'>{children}</body>
     </html>
   )
